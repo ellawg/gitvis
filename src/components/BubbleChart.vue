7b63a8ce52d7f1623cb1ef2e1567addb9335b8b8
Ella Wirstad <ella.wg@hotmail.com>
2019-01-02 13:05:50 +0100

    add bc function that runs after mount + comment

--- src/components/BubbleChart.vue
+++ src/components/BubbleChart.vue
@@ -1,12 +1,17 @@
 <template>
     <div class="bubble-chart">
-        
+
     </div>
 </template>
 
 <script>
 export default {
   name: "BubbleChart",
+  mounted: function(){
+      // runs when this component is mounted on the page
+      // this.el refers to first element inside template tag
+
+  }
 };
 </script>
 
