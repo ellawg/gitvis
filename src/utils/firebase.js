2b24c6db5b0aa997ef4840f33281d8f607b78b1b
Axel Ekwall <axel@minimize.se>
2019-01-02 22:34:34 +0100

    fix firebase error

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -9,7 +9,5 @@ const auth = fb.auth();
 
 // Set up github auth provider
 const githubProvider = new firebase.auth.GithubAuthProvider();
-githubProvider.addScope("repo");
-githubProvider.addScope("user");
 
 export { auth, githubProvider };
