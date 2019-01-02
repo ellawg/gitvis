f64185ff2c2097ec76a42f552d8358701471d15a
Ella Wirstad <ella.wg@hotmail.com>
2019-01-02 12:45:06 +0100

    init charts route w/ bc component

---
+++ src/components/BubbleChart.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="bubble-chart">
+        
+    </div>
+</template>
+
+<script>
+export default {
+  name: "BubbleChart",
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/views/Charts.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="charts">
+      <bubble-chart>
+
+      </bubble-chart>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import BubbleChart from "@/components/BubbleChart.vue";
+
+export default {
+  name: "charts",
+  components: {
+    BubbleChart
+  }
+};
+</script>
+
+<style scoped lang="scss">
+
+</style>
