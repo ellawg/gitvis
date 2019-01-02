77fbb8f1da468061697a6509465e05ade1cbb311
Ella Wirstad <ella.wg@hotmail.com>
2019-01-02 13:18:16 +0100

    bug fix

--- src/components/BubbleChart.vue
+++ src/components/BubbleChart.vue
@@ -1,5 +1,6 @@
 <template>
     <div class="bubble-chart">
+        hej
 
     </div>
 </template>
--- src/router.js
+++ src/router.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
-import BubbleChart from "./views/Charts.vue"
+import Charts from "./views/Charts.vue"
 
 Vue.use(Router);
 
