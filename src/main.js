336abb1bd0055bcec1d7d798b7bb1205fe8833d0
Axel Ekwall <axel@minimize.se>
2019-01-03 20:27:19 +0100

    add vue material

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from "vue";
+import VueMaterial from "vue-material";
+import "vue-material/dist/vue-material.min.css";
+
 import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import { createProvider } from "./vue-apollo";
+
+Vue.use(VueMaterial);
 
 Vue.config.productionTip = false;
 
