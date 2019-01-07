8dfb134d49c2f751739e854ab6a42afe33b60a1a
Axel Ekwall <axel@minimize.se>
2019-01-07 20:23:46 +0100

    remove some elements from the search component

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -3,32 +3,20 @@
     <form
       form
       novalidate
-      class="md-layout md-gutter md-alignment-top-center"
+      class="md-layout md-gutter md-alignment-canter-center"
       @submit.prevent="submit"
     >
-      <md-card class="md-layout-item md-xsmall-size-100 md-size-80">
-        <md-card-header>
-          <div class="md-title">Search</div>
-        </md-card-header>
+      <md-card class="md-layout-item">
         <md-card-content>
           <div class="md-layout md-gutter">
-            <div class="md-layout-item md-xsmall-size-100 md-size-70">
+            <div class="md-layout-item">
               <md-field md-inline>
                 <label>query</label>
                 <md-input v-model="query"></md-input>
               </md-field>
             </div>
-            <div class="md-layout-item md-xsmall-size-100 md-size-30">
-              <md-field>
-                <label>Number of results</label>
-                <md-input v-model="nrOfResults" type="number"></md-input>
-              </md-field>
-            </div>
           </div>
         </md-card-content>
-        <md-card-actions>
-          <md-button type="submit">GO!</md-button>
-        </md-card-actions>
       </md-card>
     </form>
     <router-view></router-view>
@@ -40,8 +28,7 @@ export default {
   name: "search",
   data() {
     return {
-      query: "",
-      nrOfResults: 100
+      query: ""
     };
   },
   methods: {
