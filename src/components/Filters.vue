c4baeceb40a2f837ce5ee395376d8f96f71a0c7d
Axel Ekwall <axel@minimize.se>
2019-01-27 22:20:55 +0100

    add slider and update filter (WIP)

--- src/components/Filters.vue
+++ src/components/Filters.vue
@@ -1,6 +1,74 @@
 <template>
-  <div>
-    filters
-    <router-link to="/search/hejsan">test</router-link>
+  <div class="md-layout md-gutter">
+    <div class="md-layout-item md-size-100 chart-card-container">
+      <md-card>
+        <md-card-header>
+          <div class="md-title">Filters</div>
+        </md-card-header>
+        <md-card-content>
+          <div class="md-layout">
+            <div class="md-layout-item">
+              <md-chip
+                v-for="topic in topics"
+                :key="topic"
+                class="md-accent"
+                md-deletable
+                @md-delete="filterToggle({type: 'topics', value: topic})"
+              >{{topic}}</md-chip>
+            </div>
+            <div class="md-layout-item">
+              <Slider :min="min" :max="max" v-model="minCount"></Slider>
+            </div>
+            <div class="md-layout-item">
+              <md-chip
+                v-for="lang in languages"
+                :key="lang"
+                class="md-primary"
+                md-deletable
+                @md-delete="filterToggle({type: 'languages', value: lang})"
+              >{{lang}}</md-chip>
+            </div>
+          </div>
+        </md-card-content>
+      </md-card>
+    </div>
   </div>
 </template>
+
+<script>
+import { mapState, mapActions, mapMutations } from "vuex";
+
+import { UPDATE_MIN_COUNT } from "../utils/mutations.js";
+
+import Slider from "@/components/Slider";
+
+export default {
+  name: "Filters",
+  components: {
+    Slider
+  },
+  data() {
+    return {
+      min: 1,
+      max: 10
+    };
+  },
+  watch: {
+    minCount(count) {
+      this[UPDATE_MIN_COUNT](count);
+    }
+  },
+  computed: {
+    ...mapState({
+      languages: state => state.filters.languages,
+      topics: state => state.filters.topics,
+      minCount: state => state.filters.minCount
+    })
+  },
+  methods: {
+    ...mapActions(["filterToggle"]),
+    ...mapMutations([UPDATE_MIN_COUNT])
+  }
+};
+</script>
+
---
+++ src/components/Slider.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="root">
+    <div class="slider-container">
+      <input class="slider" @change="onChange" type="range" :min="min" :max="max" v-model="value">
+    </div>
+    <div class="value-box">{{value}}</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Slider",
+  model: {
+    prop: "value",
+    event: "change"
+  },
+  props: {
+    min: { type: Number, default: 1 },
+    max: { type: Number, default: 100 },
+    value: { type: Number, default: 50 }
+  },
+  methods: {
+    onChange() {
+      this.$emit("change", this.value);
+    }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+.slider-container {
+  width: 70%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+}
+
+.slider {
+  width: 100%;
+}
+
+.value-box {
+  width: 30%;
+}
+</style>
